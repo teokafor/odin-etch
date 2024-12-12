064c4242935329b13180e61c389a9d8d4184ec13
Tobe Okafor <teokafor@gmail.com>
2024-12-12 17:32:36 -0600

    Implement grid generation

---
+++ main.js
@@ -0,0 +1,17 @@
+const CANVAS_SIZE = 720;
+const container = document.querySelector('.container');
+
+function createCanvas(squares) {
+    let squareSize = CANVAS_SIZE / squares;
+    let totalSquares = squares**2;
+    
+    for(let i = 0; i < totalSquares; i++) {
+            let square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            square.style['width'] = squareSize + 'px';
+            square.style['height'] = squareSize + 'px';
+            container.appendChild(square);
+    }
+}
+
+createCanvas(11);
